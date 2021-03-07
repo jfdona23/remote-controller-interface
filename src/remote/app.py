d338d74e38e3b1f61d9fe255623c5e0003a116cf
Juan Fernando Donadio <jf.donadio@gmail.com>
2021-03-06 23:53:11 -0300

    fixed arguments error

--- src/remote/app.py
+++ src/remote/app.py
@@ -10,34 +10,38 @@ app = Flask(__name__)
 servos = [pan_servo, tilt_servo]
 
 @app.route("/up")
-def move_up(self):
+def move_up():
     """Move Up"""
 
     pan_tilt.set_angle(tilt_servo, 45)
 
 
 @app.route("/down")
-def move_down(self):
+def move_down():
     """Move Down"""
 
     pan_tilt.set_angle(tilt_servo, 135)
 
 
 @app.route("/forward")
-def move_forward(self):
+def move_forward():
     """Move Forward"""
 
 
 @app.route("/backward")
-def move_backward(self):
+def move_backward():
     """Move Backward"""
 
 
 @app.route("/left")
-def move_left(self):
+def move_left():
     """Move Left"""
 
 
 @app.route("/right")
-def move_right(self):
+def move_right():
     """Move Right"""
+
+
+if __name__ == "__main__":
+    app.run()
